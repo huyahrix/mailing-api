8fbe1a176b9c84f8ac18abf784b3a26e533441dc
Huy Phạm <infjnite@gmail.com>
2020-08-01 12:11:32 +0700

    fetch api

--- Todo.md
+++ Todo.md
@@ -1,3 +1,5 @@
 #1 . Send mail
 - node-mailer
 - login with google - facebook
+- button submit fetch api
+- https://jsfiddle.net/seamusleahy/rxeuaatw/
--- config/routes.js
+++ config/routes.js
@@ -18,17 +18,3 @@ let initRoutes = (app) => {
 };
 
 module.exports = initRoutes;
-
-// var appRouter = function (app) {
-//     app.get('/api/', function (req, res) {
-//         res.status(200).send('Welcome to our restful API');
-//     });
-//     var UserController = require('../api/controllers/user/UserController');
-//     app.use('/api/user', UserController);
-//     var AuthController = require('../api/controllers/auth/AuthController');
-//     app.use('/api/auth', AuthController);
-//     var MongodbController = require('../api/controllers/mongodb/createDatabase');
-//     app.use('/api/mongodb', MongodbController);
-//     app.use('/api/mongodb', require('../api/controllers/mongodb/find'));
-// };
-// module.exports = appRouter;
